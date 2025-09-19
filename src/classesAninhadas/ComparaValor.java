418006fa3233025a8e16ecd72d5b062b21e452cb
0082138 <moisesemanuelreisdacruz@hotmail.com>
2025-09-18 22:06:29 -0300

    Implementando Comparator<T>

---
+++ src/classesAninhadas/ComparaValor.java
@@ -0,0 +1,11 @@
+package classesAninhadas;
+
+import java.util.Comparator;
+
+public class ComparaValor implements Comparator<Pedido.Item> {
+
+    @Override
+    public int compare(Pedido.Item o1, Pedido.Item o2) {
+        return 0;
+    }
+}
--- src/classesAninhadas/Pedido.java
+++ src/classesAninhadas/Pedido.java
@@ -1,5 +1,7 @@
 package classesAninhadas;
 
+import java.util.*;
+
 public class Pedido {
 
     private String id;
@@ -23,11 +25,31 @@ public class Pedido {
             System.out.println("Item: " + this.nome);
             System.out.println("Valor: " + this.valor + " reais");
         }
+
+        public int getValor() {
+            return this.valor;
+        }
     }
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         Pedido pedido1 = new Pedido("r4h2l9");
         Pedido.Item item1 = pedido1.new Item("Hamburguer", 23);
         item1.exibirDetalhes();
+
+        System.out.println();
+        Pedido.Item item2 = pedido1.new Item("Pizza", 45);
+        item2.exibirDetalhes();
+
+        List<Item> itens = new ArrayList<>();
+        itens.add(item1);
+        itens.add(item2);
+
+        Collections.sort(itens, new ComparaValor());
+
+        System.out.println("\nOrdem de valores: ");
+        for(Item i: itens) {
+            System.out.println(i.getValor() + " reais");
+        }
     }
+
 }
